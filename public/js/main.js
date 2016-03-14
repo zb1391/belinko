aa4efec529fe584251504a6e6b95915df2610028
Zac <zmb1391@gmail.com>
2016-03-14 11:00:13 +0000

    added comments

--- public/js/main.js
+++ public/js/main.js
@@ -134,6 +134,17 @@ var app = require('angular').module('app');
 app.service('MapHelper',['Api',function(Api){
   var self = this;
 
+  /**
+   * get the detail of a place
+   * @param {Scope} scope
+   * @param {Object} marker
+   *
+   * marker is a customer object created by the MarkerHelper
+   * need to surround in a $scope.$apply because the event is 
+   * registered outside of the scope.
+   * 
+   * this is used in the onClick of a marker
+   */
   this.getDetail = function($scope,marker){
     $scope.$apply(function(){
       $scope.showDetail = true;
